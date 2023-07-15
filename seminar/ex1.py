f43bcc1f4e6a696272bf3935741d75ecddffd419
Ainur Shamsutdinov <shamsut@gmail.com>
2023-07-15 22:22:06 +0300

    1 ex seminar

---
+++ seminar/ex1.py
@@ -0,0 +1,23 @@
+# Задание №1
+# ✔ Напишите функцию, которая заполняет файл
+# (добавляет в конец) случайными парами чисел.
+# ✔ Первое число int, второе - float разделены вертикальной чертой.
+# ✔ Минимальное число - -1000, максимальное - +1000.
+# ✔ Количество строк и имя файла передаются как аргументы функции.
+
+import random
+
+BEGIN = -1000
+END = 1000
+
+
+def write_to_file(num_lines: int, file_name):
+    f = open(file_name, 'a', encoding='utf-8')
+    for i in range(num_lines):
+        random_int = random.randint(BEGIN, END)
+        random_float = random.uniform(BEGIN, END)
+        f.write(f'\n{random_int}|{random_float}')
+    f.close()
+
+
+write_to_file(10, 'test.txt')
